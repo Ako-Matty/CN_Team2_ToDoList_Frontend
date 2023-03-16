5fc062dee82b409a394b8d4d006a3ced704188f3
Gareth Evans <macbook@MacBooks-MacBook-Pro.local>
2023-03-16 15:59:03 +0000

    donetodosmk2

---
+++ src/utils/index.js
@@ -0,0 +1,29 @@
+import { handleDeleteTodo } from "../../utils";
+import { deleteActiveOrDoneTodo } from "../../utils/todo";
+import "./DoneTodos.css";
+
+import Todo from "../todo/Todo";
+
+const DoneTodos = ({ doneTodos, setDoneTodos, user, setMessage }) => {
+    return ( 
+        <div className="done-todo-container">
+            {doneTodos
+            ? doneTodos.map((todo, index) => (
+            <Todo
+            key={index}
+            todo={todo}
+            setDoneTodos={setDoneTodos}
+            handleDeleteTodo={handleDeleteTodo}
+            doneTodos={doneTodos}
+            user={user}
+            deleteFunc={deleteActiveOrDoneTodo}
+            url={"donetodos/deletedonetodo"}
+            setMessage={setMessage}
+                />
+            ))
+          : null }
+        </div>
+    );
+};
+
+export default DoneTodos;
