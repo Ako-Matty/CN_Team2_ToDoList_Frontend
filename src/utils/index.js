64be3eab5ab8d2cb29ec1dc23a74123decc1971e
Gareth Evans <macbook@MacBooks-MacBook-Pro.local>
2023-03-16 16:31:46 +0000

    prob fixed slow down

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,29 +1,44 @@
-import { handleDeleteTodo } from "../../utils";
-import { deleteActiveOrDoneTodo } from "../../utils/todo";
-import "./DoneTodos.css";
+export const handleDeleteTodo = async (
+    e,
+    state,
+    setter,
+    todo,
+    deleteFunc,
+    url,
+    setMessage
+) => {
+    e.preventDefault();
 
-import Todo from "../todo/Todo";
+    const { setActiveTodos, setDoneTodos } = setter;
 
-const DoneTodos = ({ doneTodos, setDoneTodos, user, setMessage }) => {
-    return ( 
-        <div className="done-todo-container">
-            {doneTodos
-            ? doneTodos.map((todo, index) => (
-            <Todo
-            key={index}
-            todo={todo}
-            setDoneTodos={setDoneTodos}
-            handleDeleteTodo={handleDeleteTodo}
-            doneTodos={doneTodos}
-            user={user}
-            deleteFunc={deleteActiveOrDoneTodo}
-            url={"donetodos/deletedonetodo"}
-            setMessage={setMessage}
-                />
-            ))
-          : null }
-        </div>
-    );
+    const deletedTodo = await deleteFunc(todo, url);
+
+    try {
+        if (deletedTodo > 0 && url === "activetodos/deleteactivetodo"){
+            await setActiveTodos((el) =>
+            state.activeTodos.filter((el) =>el !== todo)
+            );
+            setMessage("Active Todo Deleted");
+        } else if (deletedTodo > 0 && url === "donetodos/deletedonetodo") {
+            await setDoneTodos((el) => state.doneTodos.filter((el) => el !== todo));
+            setMessage("Done Todo Deleted");
+        } else if (deletedTodo === 0 ) {
+            setMessage("Something Went Wrong");
+            throw new Error("Something Went Wrong");
+        }
+    } catch (error) {
+        console.log(error)
+    }
 };
 
-export default DoneTodos;
+export const logOut = (e, setUser, setActiveTodos, setDoneTodos) => {
+    e.preventDefault();
+
+    setUser(null);
+
+    setActiveTodos([]);
+    setDoneTodos([]);
+
+    document.cookie = 
+    "jwt_token=; expires=Thur, 01 Jan 1970 00:00:00 UTC; path=/;";
+};
