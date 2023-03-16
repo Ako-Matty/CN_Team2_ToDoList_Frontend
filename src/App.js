a79fafd5517eda195038528fda4f5c77a510e8e9
Gareth Evans <macbook@MacBooks-MacBook-Pro.local>
2023-03-16 14:46:34 +0000

    appjs done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-// import TodoContainer
+import TodoContainer from "./components/todoContainer/TodoContainer";
+import LogOrSignContainer from "./components/LogOrSign/LogOrSignContainer/LogOrSignContainer";
+import MessageContainer from "./components/messageContainer/messageContainer";
+
+import {getTokenFromCookie} from "./common";
+import{authCheck} from "./utils/user";
 
 function App() {
 
@@ -13,19 +18,63 @@ function App() {
   const [user, setUser] = useState(null);
   const [ message, setMessage ] = useState();
 
+  console.log("actions test");
+  console.log("hello");
 
+  useEffect(() => {
+    if (document.cookie) {
+      let token = getTokenFromCookie("jwt_token");
 
+      if (token === false) {
+        setUser(null);
+      } else {
+        loginWithToken(token);
+      }   }
+  }, [])
 
- 
+  const loginWithToken = async (token) => {
+    const persistantUser = await authCheck(token);
 
+    await setUser(persistantUser.user);
+    await setActiveToDos(persistantUser.activeToDos);
+    await setDoneTodos(persistantUser.doneTodos);
 
- 
+  };
+
   return (
-    <div className="appWrapper">
-      <h1>Team-2 Do Or Not To Do</h1>
-
+    <>
+    <Header user={user}/>
+    <div className="user-ops-container">
+      <LogOrSignContainer
+      user={user}
+      setUser={setUser}
+      setActiveToDos={setActiveToDos}
+      setDoneTodos={setDoneTodos}
+      />
+      <MessageContainer message={message} />
     </div>
+      <TodoContainer
+      activeTodos={activeTodos}
+      setActiveTodos={setActiveTodos}
+      doneTodos={doneTodos}
+      setDoneTodos={setDoneTodos}
+      user={user}
+      essage={message}
+      setMessage={setMessage}
+  />
+  <Footer />
+  </>
   );
-};
+}
 
 export default App;
+
+
+
+
+ 
+
+
+ 
+  
+
