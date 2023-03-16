84659c886fccc7cc3468829bc903e26edf2e5826
Gareth Evans <macbook@MacBooks-MacBook-Pro.local>
2023-03-16 14:58:35 +0000

    tododone

---
+++ src/components/todo/Todo.js
@@ -0,0 +1,69 @@
+import { handleDeleteTodo } from "../../utils";
+import { addDoneTodoDB } from "../../utils/todo";
+
+const Todo = ({
+    todo,
+    activeTodos,
+    handleTick,
+    setDoneTodos,
+    setActiveTodos,
+    doneTodos,
+    user,
+    deleteFunc,
+    url,
+    setMessage,
+    setDeletionMessage,
+
+}) => {
+    const stateSetters ={
+        setActiveTodos: setActiveTodos,
+        setDoneTodos: setDoneTodos,
+    };
+
+    const state = {
+        activeTodos: activeTodos,
+        doneTodos: doneTodos,
+
+    };
+
+    return (
+        <div>
+            <p>{todo.todo}</p>
+            {activeTodos ? (
+                <button
+                onClick={(e) =>
+                handleTick(
+                    e,
+                    doneTodos,
+                    setDoneTodos,
+                    todo,
+                    setActiveToDos,
+                    activeTodos,
+                    addDoneTodoToDb
+                )
+            }
+            >
+            Tick
+            </button>
+            ) : null}
+            <button 
+            onClick={(e) =>
+                handleDeleteTodo(
+            e,
+            state,
+            stateSetters,
+            todo,
+            deleteFunc,
+            url,
+            setMessage,
+            setDeletionMessage
+        )
+            }
+            >
+                Delete
+            </button>
+        </div>
+    );
+};
+
+export default Todo;
